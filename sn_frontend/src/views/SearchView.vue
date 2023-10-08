fbed9f7165b26795935b98736a5d155de02d47e3
LPHung <phuhung16820@gmail.com>
2023-10-08 17:29:48 +0700

    create search page template

---
+++ sn_frontend/src/views/SearchView.vue
@@ -0,0 +1,283 @@
+<template>
+  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+    <div class="main-left col-span-3">
+      <div class="bg-white border border-gray-200 rounded-lg">
+        <div class="p-4 flex space-x-4 justify-between">
+          <input
+            type="search"
+            class="p-4 w-10/12 bg-gray-100 rounded-lg"
+            placeholder="Bạn đang tìm gì?"
+          />
+          <div class="flex justify-center items-center">
+            <a href="#"><button>Tìm kiếm</button></a>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4"
+      >
+        <div class="p-4 bg-gray-200 text-center rounded-lg">
+          <img
+            src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+            alt=""
+            class="mb-6 rounded-full"
+          />
+          <p>
+            <strong>Picking phone Tom</strong>
+          </p>
+          <div class="mt-6 flex space-x-8 justify-around">
+            <p class="text-xs text-gray-500">229 người bạn</p>
+            <p class="text-xs text-gray-500">168 bài đăng</p>
+          </div>
+        </div>
+        <div class="p-4 bg-gray-200 text-center rounded-lg">
+          <img
+            src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+            alt=""
+            class="mb-6 rounded-full"
+          />
+          <p>
+            <strong>Picking phone Tom</strong>
+          </p>
+          <div class="mt-6 flex space-x-8 justify-around">
+            <p class="text-xs text-gray-500">229 người bạn</p>
+            <p class="text-xs text-gray-500">168 bài đăng</p>
+          </div>
+        </div>
+        <div class="p-4 bg-gray-200 text-center rounded-lg">
+          <img
+            src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+            alt=""
+            class="mb-6 rounded-full"
+          />
+          <p>
+            <strong>Picking phone Tom</strong>
+          </p>
+          <div class="mt-6 flex space-x-8 justify-around">
+            <p class="text-xs text-gray-500">229 người bạn</p>
+            <p class="text-xs text-gray-500">168 bài đăng</p>
+          </div>
+        </div>
+        <div class="p-4 bg-gray-200 text-center rounded-lg">
+          <img
+            src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+            alt=""
+            class="mb-6 rounded-full"
+          />
+          <p>
+            <strong>Picking phone Tom</strong>
+          </p>
+          <div class="mt-6 flex space-x-8 justify-around">
+            <p class="text-xs text-gray-500">229 người bạn</p>
+            <p class="text-xs text-gray-500">168 bài đăng</p>
+          </div>
+        </div>
+        <div class="p-4 bg-gray-200 text-center rounded-lg">
+          <img
+            src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+            alt=""
+            class="mb-6 rounded-full"
+          />
+          <p>
+            <strong>Picking phone Tom</strong>
+          </p>
+          <div class="mt-6 flex space-x-8 justify-around">
+            <p class="text-xs text-gray-500">229 người bạn</p>
+            <p class="text-xs text-gray-500">168 bài đăng</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="p-4 bg-white border border-gray-200 rounded-lg mt-4">
+        <div class="mb-6 flex items-center justify-between">
+          <div class="flex items-center space-x-6">
+            <img
+              src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+              class="w-[40px] rounded-full"
+            />
+
+            <p><strong>Picking phone Tom</strong></p>
+          </div>
+
+          <p class="text-gray-600">18 phút trước</p>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit sit
+          soluta dignissimos quasi beatae ea qui, error, eum vel veritatis
+          adipisci accusantium excepturi quas voluptates officia deleniti nam
+          dolorem? Minus!
+        </p>
+
+        <img
+          src="https://th.bing.com/th/id/OIP.5SOFKPjL7kQyUxxyYEk26wHaE9?pid=ImgDet&rs=1"
+          class="w-full rounded-lg mt-6"
+        />
+
+        <div class="my-6 flex justify-between">
+          <div class="flex space-x-6">
+            <div class="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                />
+              </svg>
+
+              <span class="text-gray-500 text-xs">82 lượt thích</span>
+            </div>
+
+            <div class="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
+                />
+              </svg>
+
+              <span class="text-gray-500 text-xs">0 bình luận</span>
+            </div>
+          </div>
+
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-right col-span-1">
+      <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">Người bạn có thể biết</h3>
+        <div class="space-y-4">
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <img
+                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+                class="w-[40px] rounded-full"
+              />
+
+              <p class="text-xs"><strong>Picking phone Tom</strong></p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Xem</button></a>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <img
+                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+                class="w-[40px] rounded-full"
+              />
+
+              <p class="text-xs"><strong>Picking phone Tom</strong></p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Xem</button></a>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <img
+                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+                class="w-[40px] rounded-full"
+              />
+
+              <p class="text-xs"><strong>Picking phone Tom</strong></p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Xem</button></a>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">Xu hướng</h3>
+
+        <div class="space-y-4">
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <p class="text-xs">
+                <strong>#tomandjerry</strong><br />
+                <span class="text-gray-500">180 bài viết</span>
+              </p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Khám phá</button></a>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <p class="text-xs">
+                <strong>#tomandjerry</strong><br />
+                <span class="text-gray-500">180 bài viết</span>
+              </p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Khám phá</button></a>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <p class="text-xs">
+                <strong>#tomandjerry</strong><br />
+                <span class="text-gray-500">180 bài viết</span>
+              </p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Khám phá</button></a>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <p class="text-xs">
+                <strong>#tomandjerry</strong><br />
+                <span class="text-gray-500">180 bài viết</span>
+              </p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Khám phá</button></a>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <p class="text-xs">
+                <strong>#tomandjerry</strong><br />
+                <span class="text-gray-500">180 bài viết</span>
+              </p>
+            </div>
+            <a href="#"><button class="btn-sm">Khám phá</button></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

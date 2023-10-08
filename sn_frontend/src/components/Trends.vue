8d11054b1d3bf21b33fb82b4e3364a7dfaa152de
LPHung <phuhung16820@gmail.com>
2023-10-08 17:44:50 +0700

    create Trends component

---
+++ sn_frontend/src/components/Trends.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="p-4 bg-white border border-gray-200 rounded-lg">
+    <h3 class="mb-6 text-xl">Xu hướng</h3>
+
+    <div class="space-y-4">
+      <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-2">
+          <p class="text-xs">
+            <strong>#tomandjerry</strong><br />
+            <span class="text-gray-500">180 bài viết</span>
+          </p>
+        </div>
+
+        <a href="#"><button class="btn-sm">Khám phá</button></a>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-2">
+          <p class="text-xs">
+            <strong>#tomandjerry</strong><br />
+            <span class="text-gray-500">180 bài viết</span>
+          </p>
+        </div>
+
+        <a href="#"><button class="btn-sm">Khám phá</button></a>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-2">
+          <p class="text-xs">
+            <strong>#tomandjerry</strong><br />
+            <span class="text-gray-500">180 bài viết</span>
+          </p>
+        </div>
+
+        <a href="#"><button class="btn-sm">Khám phá</button></a>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-2">
+          <p class="text-xs">
+            <strong>#tomandjerry</strong><br />
+            <span class="text-gray-500">180 bài viết</span>
+          </p>
+        </div>
+
+        <a href="#"><button class="btn-sm">Khám phá</button></a>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-2">
+          <p class="text-xs">
+            <strong>#tomandjerry</strong><br />
+            <span class="text-gray-500">180 bài viết</span>
+          </p>
+        </div>
+        <a href="#"><button class="btn-sm">Khám phá</button></a>
+      </div>
+    </div>
+  </div>
+</template>

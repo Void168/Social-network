ed260ba2a8ea0162181117ee705acc5b55dbecae
LPHung <phuhung16820@gmail.com>
2023-10-08 17:57:03 +0700

    create toast component

---
+++ sn_frontend/src/components/Toast.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    v-if="store.isVisible"
+    class="transition ease-in-out delay-500 duration-150 px-6 py-3 fixed right-8 rounded-xl shadow-xl"
+    :class="store.classes"
+  >
+    {{ store.messages }}
+  </div>
+</template>
+
+<script setup>
+import { useToastStore } from "@/stores/toast";
+
+const store = useToastStore();
+
+return {
+  store,
+};
+</script>

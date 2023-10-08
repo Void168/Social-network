4701b1455f1c46f1d37dd195a7c8f9f71f53cf79
LPHung <phuhung16820@gmail.com>
2023-10-08 17:39:08 +0700

    create PeopleYouMayKnow component

---
+++ sn_frontend/src/components/PeopleYouMayKnow.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">Người bạn có thể biết</h3>
+        <div class="space-y-4">
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <img
+                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+                class="w-[40px] rounded-full"
+              />
+
+              <p class="text-xs"><strong>Picking phone Tom</strong></p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Xem</button></a>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <img
+                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+                class="w-[40px] rounded-full"
+              />
+
+              <p class="text-xs"><strong>Picking phone Tom</strong></p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Xem</button></a>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <img
+                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
+                class="w-[40px] rounded-full"
+              />
+
+              <p class="text-xs"><strong>Picking phone Tom</strong></p>
+            </div>
+
+            <a href="#"><button class="btn-sm">Xem</button></a>
+          </div>
+        </div>
+      </div>
+</template>
--- sn_frontend/src/views/FeedView.vue
+++ sn_frontend/src/views/FeedView.vue
@@ -208,49 +208,7 @@
       </div>
     </div>
     <div class="main-right col-span-1 space-y-4">
-      <div class="p-4 bg-white border border-gray-200 rounded-lg">
-        <h3 class="mb-6 text-xl">Người bạn có thể biết</h3>
-        <div class="space-y-4">
-          <div class="flex items-center justify-between">
-            <div class="flex items-center space-x-2">
-              <img
-                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
-                class="w-[40px] rounded-full"
-              />
-
-              <p class="text-xs"><strong>Picking phone Tom</strong></p>
-            </div>
-
-            <a href="#"><button class="btn-sm">Xem</button></a>
-          </div>
-
-          <div class="flex items-center justify-between">
-            <div class="flex items-center space-x-2">
-              <img
-                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
-                class="w-[40px] rounded-full"
-              />
-
-              <p class="text-xs"><strong>Picking phone Tom</strong></p>
-            </div>
-
-            <a href="#"><button class="btn-sm">Xem</button></a>
-          </div>
-
-          <div class="flex items-center justify-between">
-            <div class="flex items-center space-x-2">
-              <img
-                src="https://i.pinimg.com/736x/fa/81/55/fa81555d2190e9c91a7d584ce7174a5f.jpg"
-                class="w-[40px] rounded-full"
-              />
-
-              <p class="text-xs"><strong>Picking phone Tom</strong></p>
-            </div>
-
-            <a href="#"><button class="btn-sm">Xem</button></a>
-          </div>
-        </div>
-      </div>
+      <PeopleYouMayKnow />
       <div class="p-4 bg-white border border-gray-200 rounded-lg">
         <h3 class="mb-6 text-xl">Xu hướng</h3>
 
@@ -314,4 +272,13 @@
   </div>
 </template>
 
-<script setup></script>
+<script>
+import PeopleYouMayKnow from "../components/PeopleYouMayKnow.vue";
+
+export default {
+  name: "FeedView",
+  components: {
+    PeopleYouMayKnow,
+  },
+};
+</script>

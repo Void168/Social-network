ce85a1b42c5c1248e747bca069cab808f242ffe6
LPHung <phuhung16820@gmail.com>
2023-11-14 23:03:35 +0700

    create information models

--- sn_backend/information/models.py
+++ sn_backend/information/models.py
@@ -5,3 +5,12 @@ from django.utils.timesince import timesince
 
 from account.models import User
 # Create your models here.
+class Website(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_by = models.ForeignKey(User, related_name='website', on_delete=models.CASCADE)
+    url = models.CharField(blank=True, null=True, max_length=255)
+
+class PhoneNumber(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_by = models.ForeignKey(User, related_name='phone_number', on_delete=models.CASCADE)
+    phone_number = models.CharField(blank=True, null=True, max_length=255)

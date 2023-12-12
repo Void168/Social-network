4c700c6da8892ec8d6bce6b0200c37b18ea983b9
LPHung <phuhung16820@gmail.com>
2023-12-12 21:48:02 +0700

    create get stories list api

--- sn_backend/story/api.py
+++ sn_backend/story/api.py
@@ -13,3 +13,34 @@ from .models import TextStory, MediaStory, ReactStory, StoryAttachment
 from .serializers import StoryAttachmentSerializer, ReactStorySerializer, TextStorySerializer, MediaStorySerializer, TextStoryDetailSerializer, MediaStoryDetailSerializer
 
 from notification.serializers import NotificationSerializer
+
+@api_view(['GET'])
+def text_story_list(request):
+    user_ids = [request.user.id]
+    current_user = request.user
+    friends = User.objects.get(Q(email=current_user)).friends.all()
+    
+    for user in request.user.friends.all():
+        user_ids.append(user.id)
+            
+    text_stories = TextStory.objects.filter(Q(created_by__in=list(user_ids), only_me=False) | Q(post_to__in=list(friends), only_me=False))
+    
+    serializer = TextStorySerializer(text_stories, many=True)
+    
+    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def media_story_list(request):
+    user_ids = [request.user.id]
+    current_user = request.user
+    friends = User.objects.get(Q(email=current_user)).friends.all()
+    
+    for user in request.user.friends.all():
+        user_ids.append(user.id)
+            
+    media_stories = MediaStory.objects.filter(Q(created_by__in=list(user_ids), only_me=False) | Q(post_to__in=list(friends), only_me=False))
+    
+    serializer = MediaStorySerializer(media_stories, many=True)
+    
+    return JsonResponse(serializer.data, safe=False)
+
--- sn_backend/story/urls.py
+++ sn_backend/story/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-
+    path('text-stories/',api.text_story_list, name='text_story_list'),
+    path('media-stories/',api.text_story_list, name='text_story_list'),
 ]

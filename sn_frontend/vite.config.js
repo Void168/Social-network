13346a7ad6697c75aa60b9e8704d19dd38367a41
LPHung <phuhung16820@gmail.com>
2024-02-25 22:13:52 +0700

    vite config

--- sn_frontend/vite.config.js
+++ sn_frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,40 +10,44 @@ export default defineConfig({
       template: {
         compilerOptions: {
           isCustomElement: (tag) => {
-            return tag.startsWith('emoji-') // (return true)
-          }
-        }
-      }
+            return tag.startsWith("emoji-"); // (return true)
+          },
+        },
+      },
     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   optimizeDeps: {
-    exclude: ['js-big-decimal']
+    exclude: ["js-big-decimal"],
   },
   esbuild: {
     supported: {
-      'top-level-await': true 
+      "top-level-await": true,
     },
   },
   build: {
-    target: 'esnext',
-    // outDir: '../sn_frontend',
-    chunkSizeWarningLimit:1500,
+    target: "esnext",
+    // outDir: "../sn_frontend",
+    chunkSizeWarningLimit: 1500,
     // rollupOptions: {
-    //   output:{
-    //       manualChunks(id) {
-    //           if (id.includes('node_modules')) {
-    //               return id.toString().split('node_modules/')[1].split('/')[0].toString();
-    //           }
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes("node_modules")) {
+    //         return id
+    //           .toString()
+    //           .split("node_modules/")[1]
+    //           .split("/")[0]
+    //           .toString();
     //       }
-    //   }
-  }
+    //     },
+    //   },
+    // },
   },
   preview: {
     port: 5173,
   },
-})
+});

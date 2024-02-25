8be2481d596c551d16caac491efe87f45c700156
LPHung <phuhung16820@gmail.com>
2024-02-26 00:45:28 +0700

    ...

--- sn_frontend/src/main.js
+++ sn_frontend/src/main.js
@@ -9,7 +9,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.VITE_BASE_URL || "http://127.0.0.1:8000";
+axios.defaults.baseURL = import.meta.VITE_BASE_URL;
 
 const app = createApp(App);
 const pinia = createPinia();

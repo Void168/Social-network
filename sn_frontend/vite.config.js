416cf2439579344e73d269f8df55859d9be1507c
LPHung <phuhung16820@gmail.com>
2024-02-25 22:13:00 +0700

    vite config

--- sn_frontend/vite.config.js
+++ sn_frontend/vite.config.js
@@ -31,15 +31,16 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    outDir: '../sn_frontend',
-    rollupOptions: {
-      output:{
-          manualChunks(id) {
-              if (id.includes('node_modules')) {
-                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
-              }
-          }
-      }
+    // outDir: '../sn_frontend',
+    chunkSizeWarningLimit:1500,
+    // rollupOptions: {
+    //   output:{
+    //       manualChunks(id) {
+    //           if (id.includes('node_modules')) {
+    //               return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //           }
+    //       }
+    //   }
   }
   },
   preview: {

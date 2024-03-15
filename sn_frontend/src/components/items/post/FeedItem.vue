f0b034299411e3439155baf25aac0df4c6c43fca
LPHung <phuhung16820@gmail.com>
2024-03-15 14:27:45 +0700

    create share post modal template

--- sn_frontend/src/components/items/post/FeedItem.vue
+++ sn_frontend/src/components/items/post/FeedItem.vue
@@ -193,20 +193,7 @@
         </div>
 
         <div class="flex items-center space-x-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-            />
-          </svg>
+          <ChatBubbleOvalLeftIcon class="w-6 h-6" />
 
           <RouterLink
             :to="{
@@ -216,6 +203,11 @@
             class="text-gray-500 text-xs dark:text-neutral-200"
             >{{ post?.comments_count }} bình luận</RouterLink
           >
+        </div>
+        <div class="flex items-center space-x-2 cursor-pointer" @click="openShareModal">
+          <ShareIcon class="w-6 h-6" />
+          <h5 class="text-gray-500 text-xs dark:text-neutral-200">Chia sẻ</h5>
+          <SharePostModal :show="isShareOpen" @closeShareModal="closeShareModal" :post="post"/>
         </div>
       </div>
       <div class="absolute right-4 shadow-lg rounded-lg ease-in duration-300">
@@ -286,7 +278,7 @@
                 </MenuItem>
               </div>
               <div class="py-1" v-else>
-                <MenuItem v-slot="{ active }" @click="reportPost">
+                <MenuItem v-slot="{ active }" @click="openShareModal">
                   <div
                     href="#"
                     :class="[
@@ -343,6 +335,8 @@ import {
   GlobeAsiaAustraliaIcon,
   UserGroupIcon,
   LockClosedIcon,
+  ChatBubbleOvalLeftIcon,
+  ShareIcon,
 } from "@heroicons/vue/24/outline";
 import { HeartIcon as HeartLike } from "@heroicons/vue/24/solid";
 import { useUserStore } from "../../../stores/user";
@@ -354,6 +348,7 @@ import PrivacyTooltip from "./Tooltip/PrivacyTooltip.vue";
 import TooltipProfileVue from "../profile/TooltipProfile.vue";
 import PrivacySelector from "../../dropdown/PrivacySelector.vue";
 import ImagePostModal from "../../modals/post/ImagePostModal.vue";
+import SharePostModal from "../../modals/post/SharePostModal.vue";
 
 export default (await import("vue")).defineComponent({
   props: {
@@ -376,6 +371,7 @@ export default (await import("vue")).defineComponent({
       is_private: this.post.is_private,
       only_me: this.post.only_me,
       isImagePostOpen: false,
+      isShareOpen: false,
     };
   },
 
@@ -549,6 +545,12 @@ export default (await import("vue")).defineComponent({
     closePostAdjustment() {
       this.isAdjust = false;
     },
+    openShareModal() {
+      this.isShareOpen = true;
+    },
+    closeShareModal() {
+      this.isShareOpen = false;
+    },
     getOption() {
       if (this.privacy.name === "Công khai") {
         this.is_private = false;
@@ -624,6 +626,9 @@ export default (await import("vue")).defineComponent({
     PrivacyTooltip,
     TooltipProfileVue,
     ImagePostModal,
+    ShareIcon,
+    ChatBubbleOvalLeftIcon,
+    SharePostModal,
   },
 });
 </script>
---
+++ sn_frontend/src/components/modals/post/SharePostModal.vue
@@ -0,0 +1,244 @@
+<template>
+  <TransitionRoot @click="$emit('closeShareModal')" appear as="template">
+    <Dialog as="div" class="relative z-50">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-50" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-slate-700 text-left align-middle shadow-xl transition-all relative"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-2xl font-medium leading-6 text-gray-900 dark:text-slate-200 text-center py-4"
+              >
+                Viết bài
+              </DialogTitle>
+              <XMarkIcon
+                @click="$emit('closeShareModal')"
+                class="w-10 h-10 p-1 cursor-pointer dark:hover:bg-slate-800/70 duration-75 text-gray-900 dark:text-slate-200 bg-slate-800 rounded-full right-4 absolute top-2"
+              />
+
+              <hr class="border dark:border-slate-600" />
+              <div class="p-6 text-gray-900 dark:text-slate-200 space-y-4">
+                <textarea
+                  v-model="body"
+                  class="p-4 w-full bg-gray-100 rounded-lg resize-none"
+                  cols="30"
+                  rows="2"
+                  placeholder="Bạn đang nghĩ gì?"
+                ></textarea>
+                <div
+                  id="preview-share"
+                  v-if="urlPost"
+                  class="flex relative justify-center items-center w-32 p-2 border-[1px] border-slate-400 rounded-lg my-2"
+                >
+                  <XMarkIcon
+                    class="absolute top-0 right-0 cursor-pointer w-6 h-6"
+                    @click="removeImage"
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-8 h-8"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </XMarkIcon>
+                  <img
+                    loading="lazy"
+                    :src="urlPost"
+                    class="w-full rounded-lg"
+                  />
+                </div>
+                <div class="flex justify-between items-center">
+                  <label for="doc-share">
+                    <div
+                      class="py-3 px-6 text-black bg-gray-400 dark:bg-slate-800 dark:border-slate-700 dark:text-neutral-200 dark:hover:bg-white dark:hover:text-slate-800 font-semibold rounded-lg transition-colors hover:bg-gray-600 hover:text-white cursor-pointer"
+                    >
+                      <span class="xm:text-base text-xs">Chọn ảnh</span>
+                    </div>
+                    <input
+                      type="file"
+                      ref="file"
+                      id="doc-share"
+                      name="doc-share"
+                      hidden
+                      @change="onFileChange"
+                    />
+                  </label>
+                  <PrivacySelector
+                    @getOption="getOption"
+                    :privacies="privacies"
+                    v-model="privacy"
+                    class="max-w-max"
+                    :style="''"
+                  />
+                </div>
+                <div
+                  class="border dark:border-slate-600 rounded-lg p-4 dark:text-neutral-200 space-y-2"
+                >
+                  <div class="flex gap-2 items-center">
+                    <img
+                      :src="post.created_by.get_avatar"
+                      class="w-8 h-8 rounded-full"
+                    />
+                    <div>
+                      <h5 class="font-semibold text-sm">
+                        {{ post.created_by.name }}
+                      </h5>
+                      <div
+                        class="dark:text-neutral-300 text-xs text-neutral-400 flex items-center gap-1"
+                      >
+                        <p>{{ post.created_at_formatted }} &middot;</p>
+                        <GlobeAsiaAustraliaIcon
+                          v-if="!post.only_me && !post.is_private"
+                          class="w-4 h-4"
+                        />
+                        <UserGroupIcon
+                          v-else-if="!post.only_me && post.is_private"
+                          class="w-4 h-4"
+                        />
+                        <LockClosedIcon
+                          v-else-if="post.only_me && post.is_private"
+                          class="w-4 h-4"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <h5>
+                    {{ post.body }}
+                  </h5>
+                  <div v-if="post.attachments.length">
+                    <img :src="post.attachments[0].get_image" />
+                  </div>
+                </div>
+                <div class="mt-4 flex justify-end gap-3">
+                  <button
+                    type="button"
+                    class="btn inline-flex justify-center rounded-md border border-transparent bg-emerald-400 text-white px-4 py-2 text-sm font-medium hover:bg-emerald-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 w-full"
+                    @click="$emit('sharePost')"
+                  >
+                    Chia sẻ
+                  </button>
+                </div>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script>
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
+import { XMarkIcon } from "@heroicons/vue/24/solid";
+import {
+  GlobeAsiaAustraliaIcon,
+  UserGroupIcon,
+  LockClosedIcon,
+} from "@heroicons/vue/24/outline";
+import { useUserStore } from "../../../stores/user";
+import PrivacySelector from "../../dropdown/PrivacySelector.vue";
+
+export default (await import("vue")).defineComponent({
+  components: {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    XMarkIcon,
+    PrivacySelector,
+    GlobeAsiaAustraliaIcon,
+    UserGroupIcon,
+    LockClosedIcon,
+  },
+  setup() {
+    const userStore = useUserStore();
+
+    return {
+      userStore,
+    };
+  },
+  props: {
+    isShareOpen: Boolean,
+    post: Object,
+  },
+  data() {
+    return {
+      body: "",
+      is_private: false,
+      only_me: false,
+      privacies: [
+        { name: "Công khai" },
+        { name: "Bạn bè" },
+        { name: "Chỉ mình tôi" },
+      ],
+      privacy: {
+        name: "",
+      },
+      selection: "",
+      urlPost: null,
+    };
+  },
+  methods: {
+    getOption() {
+      if (this.privacy.name === "Công khai") {
+        this.is_private = false;
+        this.only_me = false;
+      }
+      if (this.privacy.name === "Bạn bè") {
+        this.is_private = true;
+        this.only_me = false;
+      }
+      if (this.privacy.name === "Chỉ mình tôi") {
+        this.is_private = true;
+        this.only_me = true;
+      }
+    },
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.urlPost = URL.createObjectURL(file);
+    },
+
+    removeImage() {
+      this.urlPost = null;
+    },
+  },
+});
+</script>
